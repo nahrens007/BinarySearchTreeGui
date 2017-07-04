f9b820435a9941c2d1f4b0489bda1c3ce6be46cb
Nathan Ahrens <nathanmahrens@gmail.com>
2017-07-04 11:16:24 -0400

    Moved creation of properties window
    Property window was initially created (labels, layouts, etc...) in the create() function, which was called by mainwindow whenever the properties button was clicked on. This causes the property window to have to be remade every time the button is clicked. Moved the creation of the window to the constructor, that way it is only made once. Memory is not effected by this either, as the destructor isn't called until the parent window is closed, not when the child (property) window is closed.

--- BinarySearchTree/bst_properties_window.cpp
+++ BinarySearchTree/bst_properties_window.cpp
@@ -5,7 +5,37 @@ BST_Properties_Window::BST_Properties_Window()
 {
     window = new QMainWindow();
     window->setWindowTitle("Properties");
-    window->setMinimumSize(QSize(300, 200));    
+    window->setMinimumSize(QSize(300, 200));
+
+    // Create the label layout and labels, and add them all to it
+    labelLayout = new QVBoxLayout;\
+    heightLabel = new QLabel("Height:");
+    labelLayout->addWidget(heightLabel);
+
+    textAreaLayout = new QVBoxLayout;
+    heightLabelValue = new QLabel("");
+    textAreaLayout->addWidget(heightLabelValue);
+
+    containerLayout = new QHBoxLayout;
+    containerLayout->addLayout(labelLayout);
+    containerLayout->addLayout(textAreaLayout);
+
+    container = new QWidget;
+    container->setLayout(containerLayout);
+
+    scrollArea = new QScrollArea;
+    scrollArea->setWidget(container);
+
+    // Create the main layout and add all the widgets to it
+    mainLayout = new QVBoxLayout;
+    mainLayout->addWidget(scrollArea);
+    // mainLayout->addLayout(buttonLayout); // layout for ok/close button
+
+    centralWidget = new QWidget(window);
+    centralWidget->setLayout(mainLayout);
+
+    window->setCentralWidget(centralWidget);
+
     std::cout << "Properties constructor" << std::endl;
 }
 BST_Properties_Window::~BST_Properties_Window(){
@@ -45,34 +75,4 @@ void BST_Properties_Window::create(){
     std::cout << "Properties create()" << std::endl;
     window->activateWindow();
     window->setVisible(true);
-
-    // Create the label layout and labels, and add them all to it
-    labelLayout = new QVBoxLayout;\
-    heightLabel = new QLabel("Height:");
-    labelLayout->addWidget(heightLabel);
-
-    textAreaLayout = new QVBoxLayout;
-    heightLabelValue = new QLabel("");
-    textAreaLayout->addWidget(heightLabelValue);
-
-    containerLayout = new QHBoxLayout;
-    containerLayout->addLayout(labelLayout);
-    containerLayout->addLayout(textAreaLayout);
-
-    container = new QWidget;
-    container->setLayout(containerLayout);
-
-    scrollArea = new QScrollArea;
-    scrollArea->setWidget(container);
-
-    // Create the main layout and add all the widgets to it
-    mainLayout = new QVBoxLayout;
-    mainLayout->addWidget(scrollArea);
-    // mainLayout->addLayout(buttonLayout); // layout for ok/close button
-
-    centralWidget = new QWidget(window);
-    centralWidget->setLayout(mainLayout);
-
-    window->setCentralWidget(centralWidget);
-
 }
